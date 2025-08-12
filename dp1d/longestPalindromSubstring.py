7a84b195da4168831768719093584267499d8357
Fafer77 <0xFafer77@protonmail.com>
2025-08-12 14:23:00 +0200

    updated longestPalindromSUbstric to O(n^2) from not obvious O(n^3)

--- dp1d/longestPalindromSubstring.py
+++ dp1d/longestPalindromSubstring.py
@@ -10,21 +10,24 @@ letters are the same
 
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        curr_longest = 'a'
+        res_l = 0
+        res_r = 0
         n = len(s)
         for i in range(n):
             # odd length case
             l_odd, r_odd = i, i
             while l_odd >= 0 and r_odd < n and s[l_odd] == s[r_odd]:
-                curr_longest = curr_longest if len(curr_longest) > r_odd - l_odd + 1 else s[l_odd:r_odd + 1]
+                if r_odd - l_odd > res_r - res_l:
+                    res_l, res_r = l_odd, r_odd
                 l_odd -= 1
                 r_odd += 1
 
             # even length case
             l_even, r_even = i, i + 1
             while l_even >= 0 and r_even < n and s[l_even] == s[r_even]:
-                curr_longest = curr_longest if len(curr_longest) > r_even - l_even + 1 else s[l_even:r_even + 1]
+                if r_even - l_even > res_r - res_l:
+                    res_l, res_r = l_even, r_even
                 l_even -= 1
                 r_even += 1
             
-        return curr_longest
+        return s[res_l:res_r + 1]
